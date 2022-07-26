124cee324035af4cee43f0461ccef606426e480a
Elliot <hello@elliotmorris.dev>
2022-07-26 15:23:41 +0100

    fix

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,6 +1,6 @@
-const client = require('../../client')
-import Custom404 from "../404";
 
+import Custom404 from "../404";
+import {getClient} from '../../lib/sanity.server';
 
 const Post = ({post}) => {
   if(post === null){
@@ -16,7 +16,7 @@ const Post = ({post}) => {
 }
 
 export async function getStaticPaths() {
-  const paths = await client.fetch(
+  const paths = await getClient().fetch(
     `*[_type == "post" && defined(slug.current)][].slug.current`
   )
 
@@ -29,7 +29,7 @@ export async function getStaticPaths() {
 export async function getStaticProps(context) {
   // It's important to default the slug so that it doesn't return "undefined"
   const { slug = "" } = context.params
-  const post = await client.fetch(`
+  const post = await getClient().fetch(`
     *[_type == "post" && slug.current == $slug][0]
   `, { slug })
   return {
---
+++ types/post.ts
@@ -0,0 +1,10 @@
+export type PostType = {
+  date?: string;
+  description?: string;
+  image?: string;
+  imageAlt?: string;
+  slug: string;
+  author: string;
+  title: string;
+  topic: string;
+};
